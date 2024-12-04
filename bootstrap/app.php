83dc04faabccd7c1f41b013dc952ca83b5361350
Pip <92968130+codepip55@users.noreply.github.com>
2024-12-04 12:27:58 +0100

    Add trusted hosts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustHosts(at: ['localhost', 'pepijncolenbrander.com']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
